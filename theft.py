d40ffd217a2c8ffcbeb505305ab5bf30f8d2f083
greyspectrum <greyspectrum@users.noreply.github.com>
2017-06-21 09:13:33 -0400

    Fix subshell issue.

--- theft.py
+++ theft.py
@@ -1,22 +1,52 @@
 #!/usr/bin/env/ python3
 
 import subprocess
+from subprocess import call
+import os
+import sys
+
+def query_yes_no(question, default="yes"):
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
+
+query_yes_no("Would you like to install dependencies? This is only necessary the first time you run this script.")
 
 # Install youtube-dl
-from subprocess import call
+
 call(["sudo", "curl", "-L", "https://yt-dl.org/downloads/latest/youtube-dl", "-o", "/usr/local/bin/youtube-dl"])
 call(["sudo", "chmod", "a+rx", "/usr/local/bin/youtube-dl"])
 
 # Install avconv
+
 call(["sudo", "apt-get", "install", "libav-tools"])
 
 # Switch to Music
-call(["cd"])
-call(["cd", "Music"])
+
+os.chdir(~/Music)
 
 # Ask user for the youtube video url
+
 link = input("Enter the url for the youtube video: ")
 
 # Download video
-from subprocess import call
+
 call(["youtube-dl", "--extract-audio", "--audio-format", "mp3", "--output", "\"%(uploader)s%(title)s.%(ext)s\"", link])
